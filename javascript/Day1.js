5b2208ad4ab61edb12a2bce32e51c12eebd0a5eb
Saumya <sgupta63077@gmail.com>
2026-06-10 14:28:29 +0530

    while

---
+++ javascript/Day1.js
@@ -0,0 +1,9 @@
+console.log(1+1);
+let b=5
+let d=7
+let e=b&&d 
+console.log(e);
+e=b||d
+console.log(e);
+e=b!=d
+console.log(e);
---
+++ javascript/practice.js
@@ -0,0 +1,73 @@
+// let a=20
+// let b=30
+// let c=15
+// let d=10
+// //let e=a+b
+// //let f=c+d
+// s=(a+b>=50)&&(c+d>=50)
+// console.log(s)
+// h=(a+b>=100)||(c+d>=100)
+// console.log(h)
+// i=(a+b<=50)&&(c+d<=50)
+// console.log(i)
+// conditional
+
+// let a=65
+// if(a>=55){
+//     console.log("Grade A")
+// }
+//  else if(a>=34){
+//     console.log("Grade B")
+// }
+// else if(a>=77){
+//     console.log("C Grade")
+// }else{
+//     console.log(fail)
+// }
+
+// Example
+// let product=1,
+// if(product===1){
+//     console.log("charger")
+// }
+// else if(product===2){
+//     console.log("mobile")
+// }
+// else if(product===3){
+//     console.log("phone")
+// }
+// else if(product==="1"){
+//     console.log("pencil")
+// }
+// else if(product==="2"){
+//     console.log("pen")
+// }
+// else{
+//     console.log("product is not find")
+// }
+ 
+
+//  loops
+//  let n=10;
+//  let fact=1;
+//  for(let i=1; i<=n; i++){
+//     fact *=i;
+//  }
+//  console.log(fact);
+
+
+
+// print all even and odd from 1-85
+// for(let i=1;i<=85;i++){
+//     if(i%2===0){
+//         console.log("even")
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+let i=0;
+while(i<=40){
+    console.log(i)
+    i++
+}
