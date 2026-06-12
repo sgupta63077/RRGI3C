e031ecd44587e2ca9b03270c756d2f71d03a27e5
Saumya <sgupta63077@gmail.com>
2026-06-12 14:59:49 +0530

    bs

--- javascript/Async.js
+++ javascript/Async.js
@@ -1,23 +1,26 @@
-function getData(dataId,getNextData){
-    setTimeout(()=> {
-        console.log("DataId : "+ dataId)
-        if(getNextData){
-            getNextData()
-        }
-    } , 2000);
-}
+// function getData(dataId,getNextData){
+//     setTimeout(()=> {
+//         console.log("DataId : "+ dataId)
+//         if(getNextData){
+//             getNextData()
+//         }
+//     } , 2000);
+// }
   
-  getData(1, () =>{
-    getData(2,()=>{
-        getData(3,()=>{
-            getData(5)
-        })
-    })
-  })
+//   getData(1, () =>{
+//     getData(2,()=>{
+//         getData(3,()=>{
+//             getData(5)
+//         })
+//     })
+//   })
 
-  console.log(1)
-  setTimeout(()=>{
-    console.log(2)
+//   console.log(1)
+//   setTimeout(()=>{
+//     console.log(2)
   
-  },0)
-  console.log(3)
+//   },0)
+//   console.log(3)
+
+
+
